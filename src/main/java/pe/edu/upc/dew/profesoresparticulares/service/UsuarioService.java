fb50622ec1b92b4306f1bfedd8b7bce88b6d93a0
hdresda <hdresda@84764825-a3aa-402f-fd84-0e6092371210>
2010-07-26 01:58:57 +0000

    Actualizando clases de calificacion de alumno

--- src/main/java/pe/edu/upc/dew/profesoresparticulares/service/UsuarioService.java
+++ src/main/java/pe/edu/upc/dew/profesoresparticulares/service/UsuarioService.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.upc.dew.profesoresparticulares.service;
+
 import java.util.List;
 import pe.edu.upc.dew.profesoresparticulares.model.Usuario;
 
@@ -13,13 +13,11 @@ import pe.edu.upc.dew.profesoresparticulares.model.Usuario;
  */
 public interface UsuarioService {
 
-     public  List<Usuario> getUsuarioProfesores();
+    public List<Usuario> getUsuarioProfesores();
 
-     public  List<Usuario> getUsuarioAlumnos();
+    public List<Usuario> getUsuarioAlumnos();
 
-     public Usuario getUsuario(String mail, String password);
+    public Usuario getUsuario(String mail, String password);
 
-
-
-
+    public Usuario getUsuario(int codUsuario);
 }
